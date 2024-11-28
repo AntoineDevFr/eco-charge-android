16b82dd74fd654af7ad7e1286b7ed992d344dbc0
ellio <elliot912@free.fr>
2024-11-28 16:23:59 +0100

    rien

--- app/src/main/java/com/example/eco_charge_android/FragmentListe.kt
+++ app/src/main/java/com/example/eco_charge_android/FragmentListe.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val STATIONS = "stations"
 
--- app/src/main/java/com/example/eco_charge_android/StationService.kt
+++ app/src/main/java/com/example/eco_charge_android/StationService.kt
@@ -3,7 +3,7 @@ package com.example.eco_charge_android
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface StationService {
